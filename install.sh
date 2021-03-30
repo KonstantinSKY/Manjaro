c516cf825c8dc38887942b3f015c5cadd991aab8
sky <sky012877@gmail.com>
2021-03-30 11:06:39 -0700

    planned modify

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
-# ./update.sh
 
+./update.sh
 pkgs_list=$(<pkgs_list)
+pkgq_aur_list=$(<pkjs_aur_list)
 
-echo Will be installed:
+echo Will be installed from official repository:
 echo $pkgs_list
 echo Installing...
 sudo pacman -Syu --needed $pkgs_list
 
+echo Will be installed from official repository:
+echo $pkgs_aur_list
+echo Installing...
 
